0c2ad5fdfd4f050d8bdb233e9596ecdd8e1bb5bb
ana-aranha <anab.aranha@gmail.com>
2022-09-02 02:47:16 -0300

    get and post functions at participants route

--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
 import express from "express";
 import cors from "cors";
+import * as dayjs from "dayjs";
+import joi from "joi";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+mongoClient.connect(() => {
+	db = mongoClient.db("batepapo-uol");
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const participants = [];
-
-app.post("/participants", (req, res) => {
-	const { name } = req.body;
-
-	if (name === "") {
-		res.sendStatus(422);
-	}
-
-	res.sendStatus(201);
+const nameSchema = joi.object({
+	name: joi.string().required(),
+	lastStatus: joi.number(),
 });
 
-app.get("/participants", (req, res) => {});
+app.post("/participants", async (req, res) => {
+	try {
+		const { name } = req.body;
+		const newUser = { name, lastStatus: Date.now() };
+
+		const validation = nameSchema.validate(newUser, { abortEarly: true });
+
+		if (validation.error) {
+			console.log(validation.error.details);
+			return res.sendStatus(422);
+		}
+
+		/* if (await db.collection("customers").find().toArray().forEach()){
+			return res.sendStatus(409)
+		} */
+
+		await db.collection("participants").insertOne(newUser);
+		res.sendStatus(201);
+	} catch (err) {
+		res.sendStatus(500);
+	}
+});
+
+app.get("/participants", async (req, res) => {
+	try {
+		const participants = await db.collection("participants").find().toArray();
+		res.send(participants);
+	} catch (err) {
+		res.sendStatus(500);
+	}
+});
 
 app.post("/messages", (req, res) => {
 	const { to, text, type } = req.body;
